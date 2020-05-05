4bbc822eb6c6747c0d91192108d7d6b4fa6cdd32
Éderson Fernandes <edersonluis.rf@gmail.com>
2020-05-05 17:00:06 -0300

    aula 01 introdução ao laravel

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/series', function () {
+    $series = [
+        'Breaking Bad',
+        'LOST',
+        'Naruto'
+    ];
+
+    $html = "<ul>";
+    foreach($series as $serie) {
+        $html .= "<li>{$serie}</li>";
+    }
+    $html .= "<ul>";
+
+    return $html;
+});
