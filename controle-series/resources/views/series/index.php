8ce0eb4be9acf9733e43ebe5d71b778afd5ea484
Éderson Fernandes <edersonluis.rf@gmail.com>
2020-06-02 21:31:17 -0300

    Aula 03: Estilizando a View

--- controle-series/resources/views/series/index.php
+++ controle-series/resources/views/series/index.php
@@ -6,12 +6,23 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Controle de Séries</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
 <body>
-    <ul>
-        <?php foreach($series as $serie): ?>
-            <li><?= $serie ?></li>
-        <?php endforeach; ?>
-    </ul>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Séries</h1>
+        </div>
+
+        <a href="#" class="btn btn-dark mb-5">Adicionar</a>
+
+        <ul class="list-group">
+            <?php foreach($series as $serie): ?>
+                <li class="list-group-item">
+                    <?= $serie ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
 </body>
 </html>
